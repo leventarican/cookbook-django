ac98b19218852d3dc988b0f3fac7b375e2ad3ca6
leventarican <levent@protonmail.com>
2019-09-01 00:27:02 +0200

    database, models, admin site integration

--- developers/polls/admin.py
+++ developers/polls/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+# register Question: these objects will should have an admin interface
+# python3.7 manage.py runserver >  http://127.0.0.1:8000/admin/polls/question/
+from .models import Question
+
+admin.site.register(Question)
--- developers/polls/models.py
+++ developers/polls/models.py
@@ -1,3 +1,22 @@
-from django.db import models
+import datetime
 
-# Create your models here.
+from django.db import models
+from django.utils import timezone
+
+# A Question has a question and a publication date.
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    def __str__(self):
+        return self.question_text
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+# A Choice has two fields: the text of the choice and a vote tally. 
+# Each Choice is associated with a Question.
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+    def __str__(self):
+        return self.choice_text
